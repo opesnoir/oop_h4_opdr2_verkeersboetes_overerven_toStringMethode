f1742ad311210cd3dc63ee8430da89046486ff7f
opesnoir <goodsurroundsme@outlook.com>
2024-05-21 20:44:10 +0200

    feat: model snelheidsboete, magic numbers nog verwijderen

--- src/model/Snelheidsboete.java
+++ src/model/Snelheidsboete.java
@@ -1,4 +1,54 @@
 package model;
 
 public class Snelheidsboete extends Verkeersboete{
+    private int gemetenSnelheid;
+    private int maximumSnelheid;
+
+    //getters/setters
+    public int getGemetenSnelheid() {
+        return gemetenSnelheid;
+    }
+
+    public void setGemetenSnelheid(int gemetenSnelheid) {
+        this.gemetenSnelheid = gemetenSnelheid;
+    }
+
+    public int getMaximumSnelheid() {
+        return maximumSnelheid;
+    }
+
+    public void setMaximumSnelheid(int maximumSnelheid) {
+        this.maximumSnelheid = maximumSnelheid;
+    }
+
+    //constructor
+    public Snelheidsboete(String naamDader, String kentekenAuto, int gemetenSnelheid, int maximumSnelheid) {
+        super(naamDader, kentekenAuto);
+        this.gemetenSnelheid = gemetenSnelheid;
+        this.maximumSnelheid = maximumSnelheid;
+    }
+
+    //methode
+    // magic numbers verwijderen
+    public void bepaalBedrag(){
+        double bedrag;
+        if (this.gemetenSnelheid > 0 & this.gemetenSnelheid <= 10){
+            bedrag = (this.gemetenSnelheid - this.maximumSnelheid) * 10;
+        } else if (this.gemetenSnelheid > 10 & this.gemetenSnelheid <= 30) {
+            bedrag = (this.gemetenSnelheid - this.maximumSnelheid) * 15;
+        } else {
+            bedrag = (this.gemetenSnelheid - this.maximumSnelheid) * 20;
+        }
+        if (bedrag > 1000 ){
+            bedrag = 1000;
+        }
+        super.setBedrag(bedrag);
+    }
+
+
+
+
+
+
+
 }
