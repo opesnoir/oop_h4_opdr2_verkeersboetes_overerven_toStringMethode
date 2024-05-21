a9554cd2951da03c850d3967ff601d73e4a551d9
opesnoir <goodsurroundsme@outlook.com>
2024-05-21 20:17:23 +0200

    opschonen model parkeerboete

--- src/model/Parkeerboete.java
+++ src/model/Parkeerboete.java
@@ -2,12 +2,6 @@ package model;
 
 public class Parkeerboete extends Verkeersboete {
     private int zone;
-    private final static int ZONE_1_INPUT = 1;
-    private final static int ZONE_2_INPUT = 2;
-    private final static int ZONE_3_INPUT = 3;
-    private final static String ZONE_1 = "zone 1";
-    private final static String ZONE_2 = "zone 2";
-    private final static String ZONE_3 = "zone 3";
     private final static double BOETE_ZONE_1 = 60.00;
     private final static double BOETE_ZONE_2 = 100.00;
     private final static double BOETE_ZONE_3 = 180.00;
@@ -26,6 +20,7 @@ public class Parkeerboete extends Verkeersboete {
     public Parkeerboete(String naamDader, String kentekenAuto, int zone) {
         super(naamDader, kentekenAuto);
         this.zone = zone;
+        bepaalBedrag(); // boete bedrag bepalen
     }
 
     //methode
@@ -36,7 +31,6 @@ public class Parkeerboete extends Verkeersboete {
             case 3 -> super.setBedrag(BOETE_ZONE_3);
         }
     }
-
 
     @Override
     public String toString() {
